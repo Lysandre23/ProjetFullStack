8d11fbd9148e196f03df5ddd247088332ab4021b
Lysandre88 <a.humbert2301@gmail.com>
2024-10-16 09:30:51 +0200

    Add admin id to center

--- back/app/src/main/java/org/example/model/Center.java
+++ back/app/src/main/java/org/example/model/Center.java
@@ -16,16 +16,18 @@ public class Center {
     private String address;
     private String phone;
     private String email;
+    private Integer adminid;
 
     public Center() {}
 
-    public Center(Integer id, String name, String city, String address, String phone, String email) {
+    public Center(Integer id, String name, String city, String address, String phone, String email, Integer adminid) {
         this.id = id;
         this.name = name;
         this.city = city;
         this.address = address;
         this.phone = phone;
         this.email = email;
+        this.adminid = adminid;
     }
 
     public Integer getId() { return id; }
@@ -40,4 +42,6 @@ public class Center {
     public void setPhone(String phone) { this.phone = phone; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+    public Integer getAdminid() { return adminid; }
+    public void setAdminid(Integer adminid) { this.adminid = adminid; }
 }
