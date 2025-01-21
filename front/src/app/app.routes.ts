ca4bae1322d7043593f64e49f77571c458af8082
gaguy <gaelle.antoine2@etu.univ-lorraine.fr>
2025-01-21 21:40:58 +0100

    delete front

--- front/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserListComponent } from './components/user-list.component';
-
-export const routes: Routes = [
-  { path: '', component: UserListComponent },  // Default route showing user list
-];
