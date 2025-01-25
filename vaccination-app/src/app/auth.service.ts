f17c3bcc9ca1a4c98d6a0d346efcfdfeefdbe45b
gaguy <gaelle.antoine2@etu.univ-lorraine.fr>
2025-01-25 08:59:57 +0100

    add deconnexion

--- vaccination-app/src/app/auth.service.ts
+++ vaccination-app/src/app/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private role: string | null = null;
+
+  constructor(private router: Router) {}
 
   setRole(role: string): void {
     this.role = role;
@@ -16,5 +19,15 @@ export class AuthService {
 
   clearRole(): void {
     this.role = null;
+  }
+
+  isLoggedIn(): boolean {
+    return this.role !== null;
+  }
+
+  logout(): void {
+    this.clearRole();
+    // Redirection vers la page de connexion après déconnexion
+    this.router.navigate(['/login']);
   }
 }
--- vaccination-app/src/app/navbar/navbar.component.ts
+++ vaccination-app/src/app/navbar/navbar.component.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common'; // Nécessaire pour *ngIf et autres directives Angular
 
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -16,7 +15,12 @@ import { CommonModule } from '@angular/common'; // Nécessaire pour *ngIf et aut
 export class NavbarComponent {
   role: string | null = null;
 
-  constructor(private authService: AuthService) {
+  constructor(public authService: AuthService) { // Rendre authService public pour le template
     this.role = this.authService.getRole(); // Obtenez le rôle actuel
+  }
+
+  logout(): void {
+    this.authService.logout();
+    // Redirection vers la page de connexion ou autre traitement
   }
 }
