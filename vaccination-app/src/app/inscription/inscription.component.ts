eb89c0ac2fc28aec9dd6964d851511563a4645ea
Lysandre88 <a.humbert2301@gmail.com>
2025-02-28 18:39:40 +0100

    yyyy

--- vaccination-app/src/app/inscription/inscription.component.ts
+++ vaccination-app/src/app/inscription/inscription.component.ts
@@ -1,44 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatInputModule } from '@angular/material/input';
-<<<<<<< HEAD
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatOptionModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SignupService } from '../services/signup.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-=======
-import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
-@Component({
-  selector: 'app-inscription',
->>>>>>> c4cfbf8924fdb2311811ca9fd6df7256ea96ccaa
   templateUrl: './inscription.component.html',
   imports: [
-<<<<<<< HEAD
     ReactiveFormsModule, CommonModule, NgIf, FormsModule, 
     MatFormFieldModule, MatInputModule, MatDatepickerModule, 
     MatNativeDateModule, MatSelectModule, MatOptionModule,
-    HttpClientModule
-=======
-    MatTabsModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    FormsModule
->>>>>>> c4cfbf8924fdb2311811ca9fd6df7256ea96ccaa
   ],
   styleUrls: ['./inscription.component.css']
 })
-<<<<<<< HEAD
 export class InscriptionComponent {
   inscriptionForm: FormGroup;
+  inscriptionMedecinForm: FormGroup;
   isLoading = false;
   error: string | null = null;
 
@@ -50,24 +38,6 @@ export class InscriptionComponent {
     this.inscriptionForm = this.fb.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
-=======
-
-export class InscriptionComponent implements OnInit {
-  inscriptionPatientForm!: FormGroup;
-  inscriptionMedecinForm!: FormGroup;
-  centres = [
-    { id: 1, nom: 'Centre Médical Paris' },
-    { id: 2, nom: 'Clinique Lyon Sud' },
-    { id: 3, nom: 'Hôpital de Marseille' }
-  ];
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.inscriptionPatientForm = this.fb.group({
-      nom: ['', Validators.required],
-      prenom: ['', Validators.required],
->>>>>>> c4cfbf8924fdb2311811ca9fd6df7256ea96ccaa
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       birthdate: ['', Validators.required],
@@ -84,7 +54,6 @@ export class InscriptionComponent implements OnInit {
     });
   }
 
-<<<<<<< HEAD
   onSubmit() {
     if (this.inscriptionForm.valid) {
       this.isLoading = true;
@@ -114,19 +83,6 @@ export class InscriptionComponent implements OnInit {
       });
     } else {
       this.error = 'Veuillez remplir correctement tous les champs.';
-=======
-  onSubmitPatient() {
-    if (this.inscriptionPatientForm.valid) {
-      console.log('Inscription patient:', this.inscriptionPatientForm.value);
-      // Ajouter la logique pour envoyer les données au backend
-    }
-  }
-
-  onSubmitMedecin() {
-    if (this.inscriptionMedecinForm.valid) {
-      console.log('Inscription médecin:', this.inscriptionMedecinForm.value);
-      // Ajouter la logique pour envoyer les données au backend
->>>>>>> c4cfbf8924fdb2311811ca9fd6df7256ea96ccaa
     }
   }
 }
